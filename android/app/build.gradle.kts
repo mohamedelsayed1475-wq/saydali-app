1c552349a8e6b9c1ae7cb2417b09884601312fc2
mohamedelsayed1475-wq <mohamedelsayed1475@gmail.com>
2026-05-10 04:42:30 +0300

    Update build config and ignore list

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.saydali.pro"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
